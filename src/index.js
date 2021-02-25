464d87845587a6bd940d27e41b6776dfb6e47a2e
Anna_L <Lavrinchuk@yahoo.comgit config --listgit config --global user.email Lavrinchuk@yahoo.com>
2021-02-25 23:07:58 +0200

    feat: "Towel Sort solution"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix = 0) {
+    if (matrix.length == 0 || arguments.length == 0) {
+        return []
+    } else {
+        let a = matrix.concat();
+        let c = [];
+        a.forEach(function (item, i, a) {
+            let b = 0;
+            if (!(i % 2 == 0)) {
+                item.reverse();
+                b = new Map([a]).join(',');
+                b = a.join(',');
+                return c = b.split(',').map(Number);
+            }
+        });
+return c
+    }
 }
